2e238fd599e929c96f484f60a7b4d1b626feccdc
Ayush <113465008+ayush1944@users.noreply.github.com>
2025-08-03 00:17:27 +0530

    doni

--- server/index.js
+++ server/index.js
@@ -10,18 +10,17 @@ connectDB();
 
 const app = express();
 
-app.use(cors(
-    {
-        origin: process.env.CLIENT_URL,
-        credentials: true
-    }
-));
+app.use(cors({
+  origin: process.env.CLIENT_URL ,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true // only if you're sending cookies/auth headers
+}));
 app.use(express.json());
 
 app.use('/api', apiRoutes);
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
